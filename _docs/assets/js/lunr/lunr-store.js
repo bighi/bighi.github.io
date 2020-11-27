e96d67e49ddcd0e9ec26868b7e0492487ff25d68
Leonardo Bighi <contato@leonardobighi.com>
2020-11-27 00:02:18 -0300

    Adds Docs folder

---
+++ _docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "New Blog",
+        "excerpt":"New blog. Again. I have created and abandoned many blogs in the latest years. These days I feel sad for not keeping them (it would be nice to look back to how I used to think years ago). Now I’ve decided to try one more time. I’ll try a different...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/new-blog/",
+        "teaser": null
+      },{
+        "title": "31st birthday and life goals",
+        "excerpt":"Yesterday was my 31st birthday. This is weird. I’m not used to thinking about myself as a thirty-something. Instead of celebrating, I stopped to reflect about life, the universe and everything. But mostly I reflected about all the objectives I had for my life when I was in my early...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/31st-birthday-and-life-goals/",
+        "teaser": null
+      },{
+        "title": "Why I prefer plaintext",
+        "excerpt":"I’ve used many different apps to organize my notes and documents. I’ve used Simplenote, Evernote, Google Docs, and many others. And every time I end up coming back to the simplest solution of all: plain text files inside a document folder. It doesn’t matter how advanced technology is, sometimes the best solution is...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/why-i-prefer-plaintext/",
+        "teaser": null
+      },{
+        "title": "How I organize my life in plaintext",
+        "excerpt":"Yesterday I talked about my dislike for proprietary formats for my notes. Now it’s time to explain how I organize my notes, documents, tasks, etc. If you didn’t read the previous post, here’s the summary: I use only plain text for my notes and tasks, saved in a Documents directory...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/how-i-organize-my-life-in-plain-text/",
+        "teaser": null
+      }]
